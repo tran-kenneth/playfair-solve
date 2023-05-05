438e347e18779fbe0f58c5ccd47b97c0f4991127
Kenneth <trank.dev@gmail.com>
2023-05-05 11:18:12 -0400

    Add cell class for playfair square

--- playfair.py
+++ playfair.py
@@ -1,12 +1,8 @@
-# I = J
-
 
 class Square():
     def __init__(self, key="ABCDEFGHIKLMNOPQRSTUVWXYZ"):
 
         self.key = key
-        self.key_square = self.generate_square(key)
-        ...
 
     def __str__(self):
         # TODO
@@ -16,26 +12,6 @@ class Square():
     def clean_key(self, key):
         return key.replace(" ", "").upper()
 
-    def generate_square(self, key):
-        PF_ALPHA = "ABCDEFGHIKLMNOPQRSTUVWXYZ"  # I = J
-
-        # Generate alphabetical list of chars
-        alpha_list = []
-        for char in PF_ALPHA:
-            alpha_list.append(char)
-
-        # Generate list of chars from key to make set from
-        key_alpha_list = []
-        for char in PF_ALPHA:
-            key_alpha_list.append(key)
-
-        key_set = set(key_alpha_list)
-
-        return key_set
-
-    def display_square(self):
-        ...
-
     @property
     def key(self):
         return self._key
@@ -44,10 +20,49 @@ class Square():
     def key(self, key_str):
         self._key = key_str
 
-    @property
-    def key_square(self):
-        return self._key_square
 
-    @key_square.setter
-    def key_square(self, key_str):
-        self._key_square = self.generate_square(key_str)
+class Cell():
+
+    def __init__(self, x, y, letter="Placeholder"):
+
+        self.x = x
+        self.y = y
+        self.letter = letter
+
+    def __str__(self):
+        return f'{self.letter} @ [{self.x}, {self.y}]'
+
+    def same_row(self, cell_2):
+        return self.y == cell_2.y
+
+    def same_column(self, cell_2):
+        return self.x == cell_2.x
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, new_x):
+        self._x = new_x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, new_y):
+        self._y = new_y
+
+    @property
+    def letter(self):
+        return self._letter
+
+    @letter.setter
+    def letter(self, new_letter):
+        self._letter = new_letter
+
+
+cell1 = Cell(3, 4)
+cell2 = Cell(2, 4)
+print(cell1.same_row(cell2))
